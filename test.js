5a9a8598d80d9b46a2c5e0aae6e0822d597718cf
denissg1 <denis.gavrin@gmail.com>
2015-11-24 16:17:18 +0200

    inSchool

--- test.js
+++ test.js
@@ -1,21 +1,32 @@
 	
 function numbers() {
-	var a = prompt('Введите первое число', ''), b = prompt('Введите второе число', ''), c = 0;
-		
+	
+	var a,b,c;
+	if(a = prompt('Введите первое число', '')==null){
+		alert("Операция отменена");
+		exit;
+	}
+	if(b = prompt('Введите второе число', '')==null){
+		alert("Операция отменена");
+ 		exit;
+ 	}
 		if(a <= b) {
 			for (; a <= b; a++) {
 			if (a % 2 > 0) {
-				c = +a + +c
+				c += +a;
 			}
 		
 		}} else if (a => b) {
 			for (; b <= a; b++) {
 			if (b % 2 > 0) {
-				c = +b + +c
+				c += +b
 			}
 		
 		}}
 alert(c)
+}
+function func() {
+	alert('Hello world');
 }
 
 numbers()
