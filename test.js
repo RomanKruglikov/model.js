8e29162c6f78cdbda606028365105b966eac642a
pupil <pupil@Kruglikov>
2015-12-01 16:21:03 +0200

    01.12school

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 	c +=b;
 }
 alert(c)*/
-
+//______________________________________________________________
 /*function sumTo(n) {
 	if (n != 1) {
 		return n + sumTo(n - 1);
@@ -18,7 +18,7 @@ alert(sumTo(100))*/
 	c *= b;
 }
 alert(c)*/
-
+//_____________________________________________________________
 /*function calcFactorial(n) {
 	if (n != 1) {
 		return n * calcFactorial(n - 1);
@@ -27,11 +27,28 @@ alert(c)*/
 	}
 }
 alert(calcFactorial(5))*/
-
-function fib(n) {
+//>>>>>>>>>>>>>>>>>>>>ФИБОНАЧИ<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/*function fib(n) {
 	if (n > 1) {return fib(n - 1) + fib(n - 2);
 	} else {
 		return n;
 	}
 }
-alert( fib(8) )
+alert( fib(4) )*/
+//___________________________________________________________
+
+function fib(){
+	var a = +prompt("?", "");
+	if ( a > 3 ) {
+	for (var b = 1, c = 2,d; a > 3; a--) {
+	d = b + c;
+	b = c, c = d;
+}} else if (a === 3) {
+	d = 2;
+} else if (a === 1 || a === 2) {
+	d = 1;
+} else {
+	d = "number incorrect";
+}
+alert("**_" + d  + "_**");
+}
